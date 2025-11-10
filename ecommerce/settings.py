821091222d85ed065b46d7a3c106fb73f1b03e05
siddu <siddusidharth6300@gmail.com>
2025-11-10 17:51:56 +0530

    Added gunicorn to requirements

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,6 +78,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
